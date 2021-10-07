3b0f5cbe0527d859edc3aa4e7c1e695c5554088f
Sai Jagadeesh <45138805+jagadeesh93098@users.noreply.github.com>
2021-10-07 16:04:57 +0530

    Delete Simmulated_Annealing directory

--- Simmulated_Annealing/Total_Loan_Loss.m
+++
@@ -1,8 +0,0 @@
-function k=Total_Loan_Loss(x)
-    % Array containing Loan Sizes
-    L=[10 25 4 11 18 3 17 15 9 10];    
-    
-    % Expected Loan Loss (Lambda from Credit Rating)
-    lamda=[0.0002 0.0058 0.0001 0.0003 0.0024 0.0002 0.0058 0.0002 0.001 0.001];
-    k=(L.*lamda)*x.';
-end
